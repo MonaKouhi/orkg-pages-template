01bac38676781ff56007c981f356b33c8a3d82c6
MonaKouhi <mona.mk93@gmail.com>
2021-08-04 00:01:09 +0430

    Updates

---
+++ precache-manifest.325b6ddaa101d8a5b9b2df15129092e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0c01be23dbbc441645aa8bcd579182",
+    "url": "./index.html"
+  },
+  {
+    "revision": "904c1194d1c6c1b6df11",
+    "url": "./static/css/2.78063165.chunk.css"
+  },
+  {
+    "revision": "9a3304013c8ebeaed466",
+    "url": "./static/css/main.b04fd904.chunk.css"
+  },
+  {
+    "revision": "904c1194d1c6c1b6df11",
+    "url": "./static/js/2.25dd79b0.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "./static/js/2.25dd79b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3304013c8ebeaed466",
+    "url": "./static/js/main.02b5738d.chunk.js"
+  },
+  {
+    "revision": "0bb4eb37e09b80c79523",
+    "url": "./static/js/runtime-main.7f568536.js"
+  },
+  {
+    "revision": "7d352eef4fc0d24048db63b8a4644f4e",
+    "url": "./static/media/logo.7d352eef.svg"
+  },
+  {
+    "revision": "568758605e0b7e73ae1470cee89799f0",
+    "url": "./static/media/logo_white.56875860.svg"
+  }
+]);
